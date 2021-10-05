b12d3fcdb62400d606ae33e76df2b6085ab26817
Simon Whelan <simon.whelan@fdmgroup.com>
2021-10-05 17:08:21 +1300

    initial commit

---
+++ Lettuce/src/test/java/com/fdmgroup/Lettuce/LettuceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.Lettuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
