761540c5e1d4ad9e19ca42ec1f924b3a96b0ff86
willa fan <willa.fan@fdmgroup.com>
2021-10-05 12:50:22 +0000

    Portfolio

---
+++ Lettuce/src/main/java/com/fdmgroup/Lettuce/Service/iPortfolio.java
@@ -0,0 +1,18 @@
+package com.fdmgroup.Lettuce.Service;
+
+import java.util.List;
+
+import com.fdmgroup.Lettuce.Models.Currency;
+import com.fdmgroup.Lettuce.Models.Portfolio;
+
+public interface iPortfolio {
+	List<Portfolio> getAllPortfolios();
+	Portfolio getPortfolioById(int portfolio_id);
+	void addPortfolio(Portfolio portfolio);
+	void deletePortfolio(Portfolio portfolio);
+	void deletePortfolioById(int portfolio_id);
+	void deleteAllPortfolios();
+	
+	void increaseCurrency(Currency currency, Double quantity, int portfolio_id);
+	void decreaseCurrency(Currency currency, Double quantity, int portfolio_id);
+}
