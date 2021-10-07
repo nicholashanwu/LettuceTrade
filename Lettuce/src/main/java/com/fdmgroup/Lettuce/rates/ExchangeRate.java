ec6f3fd39620617288642e8510a30ff46205f8f2
Simon Whelan <simon.whelan@fdmgroup.com>
2021-10-07 18:16:16 +1300

    Added popular rates filter

--- Lettuce/src/main/java/com/fdmgroup/Lettuce/rates/ExchangeRate.java
+++ Lettuce/src/main/java/com/fdmgroup/Lettuce/rates/ExchangeRate.java
@@ -5,8 +5,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -16,14 +16,18 @@ import com.google.gson.reflect.TypeToken;
 public class ExchangeRate {
 
 	private static final String baseUrl = "https://v6.exchangerate-api.com/v6/7e45aa311d560c1d005ccfa4/";
+	
+	private static final String popularCurrencies = "USD|EUR|JPY|GBP|AUD";
 
 	/**
+	 * Contacts external API and gets the conversion rate between two currencies.
 	 * 
 	 * @param baseCurrency   the currency you are spending, as a 3-letter string.
 	 * @param targetCurrency the currency you are buying, as a 3-letter string.
 	 * @return a double for how much of the target currency you can buy with 1 unit
 	 *         of the base currency.
-	 * @throws IOException          if there is a connection failure, or if an invalid currency code is passed.
+	 * @throws IOException if there is a connection failure, or if an invalid
+	 *                     currency code is passed.
 	 */
 	public static double getRateForPair(String baseCurrency, String targetCurrency) throws IOException {
 		// Build URL
@@ -42,7 +46,16 @@ public class ExchangeRate {
 		double rate = resultJson.get("conversion_rate").getAsDouble();
 		return rate;
 	}
-	
+
+	/**
+	 * Contacts external API and gets the values of all world currencies compared to
+	 * a base currency.
+	 * 
+	 * @param baseCurrency the currency you are spending, as a 3-letter string.
+	 * @return a map of all the currency names and their values.
+	 * @throws IOException if there is a connection failure, or if an invalid
+	 *                     currency code is passed.
+	 */
 	public static Map<String, Double> getAllRates(String baseCurrency) throws IOException {
 		// Build URL
 		String urlStr = baseUrl + "latest/" + baseCurrency;
@@ -57,7 +70,29 @@ public class ExchangeRate {
 		JsonObject resultJson = JsonParser.parseReader(resultStream).getAsJsonObject();
 
 		// Retrieve data
-		HashMap<String, Double> rates = new Gson().fromJson(resultJson.get("conversion_rates"), new TypeToken<HashMap<String, Double>>(){}.getType());
+		TreeMap<String, Double> rates = new Gson().fromJson(resultJson.get("conversion_rates"),
+				new TypeToken<TreeMap<String, Double>>() {
+				}.getType());
 		return rates;
+	}
+
+	/**
+	 * Contacts external API and gets the values of a hardcoded selection of popular
+	 * currencies compared to a base currency.
+	 * 
+	 * @param baseCurrency the currency you are spending, as a 3-letter string.
+	 * @return a map of all the currency names and their values.
+	 * @throws IOException if there is a connection failure, or if an invalid
+	 *                     currency code is passed.
+	 */
+	public static Map<String, Double> getPopularRates(String baseCurrency) throws IOException {
+		Map<String, Double> rates = getAllRates(baseCurrency);
+		Map<String, Double> filteredRates = new TreeMap<>();
+		for (Map.Entry<String, Double> rate : rates.entrySet()) {
+			if (rate.getKey().matches(popularCurrencies)) {
+				filteredRates.put(rate.getKey(), rate.getValue());
+			}
+		}
+		return filteredRates;
 	}
 }
--- Lettuce/src/test/java/com/fdmgroup/Lettuce/rates/ExchangeRateTest.java
+++ Lettuce/src/test/java/com/fdmgroup/Lettuce/rates/ExchangeRateTest.java
@@ -9,40 +9,42 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class ExchangeRateTest {
-	
+
 	@Test
-	public void test_getPair_validInput() {
-		try {
-			double rate = ExchangeRate.getRateForPair("USD", "EUR");
-			
-			assertThat(rate).isBetween(0.8, 0.9); // This is not guaranteed true, but the exchange rate has been within this range for more than a year
-			
-			System.out.println(rate);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void test_getPair_validInput() throws IOException {
+		double rate = ExchangeRate.getRateForPair("USD", "EUR");
+
+		assertThat(rate).isBetween(0.8, 0.9); // This is not guaranteed true, but the exchange rate has been within this
+												// range for more than a year
+
+		System.out.println(rate);
 	}
-	
+
 	@Test
 	public void test_getPair_invalidInput() {
-		assertThrows(IOException.class, ()->{ExchangeRate.getRateForPair("nonsense", "gibberish");});
+		assertThrows(IOException.class, () -> {
+			ExchangeRate.getRateForPair("nonsense", "gibberish");
+		});
 	}
-	
+
 	@Test
-	public void test_getAll_validInput() {
-		try {
-			Map<String, Double> rates = ExchangeRate.getAllRates("USD");
-			
-			assertThat(rates).isNotEmpty();
-			
-			double rate = rates.get("EUR");
-			assertThat(rate).isBetween(0.8, 0.9);
-			
-			System.out.println(rates);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void test_getAll_validInput() throws IOException {
+		Map<String, Double> rates = ExchangeRate.getAllRates("USD");
+
+		assertThat(rates).isNotEmpty();
+
+		double rate = rates.get("EUR");
+		assertThat(rate).isBetween(0.8, 0.9);
+
+		System.out.println(rates);
+	}
+
+	@Test
+	public void test_getPopular() throws IOException {
+		Map<String, Double> rates = ExchangeRate.getPopularRates("USD");
+
+		assertThat(rates).hasSize(5);
+		
+		System.out.println(rates);
 	}
 }
