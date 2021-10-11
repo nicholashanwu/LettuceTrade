137a19867a5d7c3c7782b7d8903eccc158a51bda
Simon Whelan <simon.whelan@fdmgroup.com>
2021-10-12 12:10:35 +1300

    Converted heldCurrencies to eager fetch type

--- Lettuce/src/main/java/com/fdmgroup/Lettuce/Models/Portfolio.java
+++ Lettuce/src/main/java/com/fdmgroup/Lettuce/Models/Portfolio.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,7 +25,7 @@ public class Portfolio {
 	@JoinColumn(name = "FK_user")
 	private User user;
 	
-	@OneToMany(mappedBy="portfolio")
+	@OneToMany(mappedBy="portfolio", fetch = FetchType.EAGER)
 	private List<HeldCurrency> heldCurrencies = new ArrayList<>();
 	
 	public Portfolio() {
