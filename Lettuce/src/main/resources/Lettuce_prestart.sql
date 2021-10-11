deadcb9fe20f10c7dc39b3fbb8326efa0516db57
Simon Whelan <simon.whelan@fdmgroup.com>
2021-10-11 23:42:24 +1300

    SQL file for loading default currencies

---
+++ Lettuce/src/main/resources/Lettuce_prestart.sql
@@ -0,0 +1,10 @@
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('USD', 'United States dollar');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('EUR', 'Euro');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('JPY', 'Japanese yen');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('GBP', 'Pound sterling');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('AUD', 'Australian dollar');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('CAD', 'Canadian dollar');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('CHF', 'Swiss franc');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('CNY', 'Chinese yuan');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('HKD', 'Hong Kong dollar');
+INSERT INTO lettuce_currency (currency_code, full_name) VALUES('NZD', 'New Zealand dollar');
