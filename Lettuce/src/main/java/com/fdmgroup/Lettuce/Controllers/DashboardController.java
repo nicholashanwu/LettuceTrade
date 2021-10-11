088ef7a70c46aec0f2ec93af2b8ded014fed4c4e
alex pun <alex.pun@fdmgroup.com>
2021-10-11 12:36:46 +1100

    Merge nick

--- Lettuce/src/main/java/com/fdmgroup/Lettuce/Controllers/DashboardController.java
+++ Lettuce/src/main/java/com/fdmgroup/Lettuce/Controllers/DashboardController.java
@@ -6,11 +6,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.fdmgroup.Lettuce.Models.Currency;
 import com.fdmgroup.Lettuce.Models.HeldCurrency;
@@ -23,6 +27,7 @@ import com.fdmgroup.Lettuce.Service.UserServiceImpl;
 import com.fdmgroup.Lettuce.rates.ExchangeRate;
 
 @Controller
+@SessionAttributes({"user"})
 public class DashboardController {
 
 	
@@ -32,12 +37,12 @@ public class DashboardController {
 	UserServiceImpl usi = new UserServiceImpl();
 	
 	
-	@RequestMapping("/")
-	public String indexPage() {
-//		actLogger.info("Landed in Home Page")
-		return "index";
-	}
-	
+//	@RequestMapping("/")
+//	public String indexPage() {
+////		actLogger.info("Landed in Home Page")
+//		return "index";
+//	}
+//	DUPLICATE
 
 
 	
@@ -53,7 +58,7 @@ public class DashboardController {
 		//Usercontoller need to set attibrute and pass it 
 		//Portfolio portfolio = usi.getUserById(userId).getPortfolio();
 		
-		
+
 		//hardcode it just for test
 		Portfolio portfolio = new Portfolio();
 		portfolio.setUser(usi.getUserById(121));
@@ -67,9 +72,7 @@ public class DashboardController {
 		// show first 4 heldcurrency
 		model.addAttribute("heldCurrencys", heldcurrency); // each has attribute currency, quantity
 
-		
-		
-		
+
 //		//show the popular rates with AUD
 		Map<String, Double> rates = new HashMap<String, Double>();
 		String currency ="AUD";
@@ -101,11 +104,13 @@ public class DashboardController {
 		return "dashboard";
 	}
 	
-	@RequestMapping("/placeOrder")
+	@RequestMapping("/placeOrderHandler")
 	public String toplaceOrderPage(Model model){
 		Order order = new Order();
 		model.addAttribute("order",order);
 		return "placeOrder";
 	}
+	
+	
 	
 }
