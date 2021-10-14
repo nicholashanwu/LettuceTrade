0e10c2e68952107ec5f566b5b9c428c4d4a9df62
Simon Whelan <simon.whelan@fdmgroup.com>
2021-10-14 20:04:16 +1300

    Reduced API calls in OutstandingController

--- Lettuce/src/main/java/com/fdmgroup/Lettuce/Controllers/OutstandingController.java
+++ Lettuce/src/main/java/com/fdmgroup/Lettuce/Controllers/OutstandingController.java
@@ -6,8 +6,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
@@ -22,6 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.fdmgroup.Lettuce.Exceptions.InsufficientFundsException;
 import com.fdmgroup.Lettuce.Exceptions.InvalidDateException;
 import com.fdmgroup.Lettuce.Exceptions.RecursiveTradeException;
+import com.fdmgroup.Lettuce.Models.Currency;
 import com.fdmgroup.Lettuce.Models.Order;
 import com.fdmgroup.Lettuce.Models.OrderStatus;
 import com.fdmgroup.Lettuce.Models.OrderType;
@@ -41,14 +44,32 @@ public class OutstandingController {
 	public static final Logger actLogger=LogManager.getLogger("ActLogging");
 	public static final Logger dbLogger=LogManager.getLogger("DBLogging");
 	
+	// Helper method
+	private Map<Currency, Double> getFilteredRates(String currencyCode) throws IOException {
+		Map<String, Double> ratesAsStrings = ExchangeRate.getAllRates(currencyCode);
+		Map<Currency, Double> ratesAsObjects = new HashMap<>();
+		for (Map.Entry<String, Double> rate : ratesAsStrings.entrySet()) {
+			try {
+				Currency currency = csi.getCurrencyById(rate.getKey());
+				ratesAsObjects.put(currency, rate.getValue());
+			} catch (EntityNotFoundException e) {
+				// API has given us a currency that we're not tracking in our database.
+				// Don't add this one to the map. Do nothing.
+			}
+		}
+		return ratesAsObjects;
+	}
+	
 	@RequestMapping("/outstandingOrder")
 	public String outOrderPage(Model model, HttpServletRequest request) throws IOException {
 	User user = (User) request.getSession().getAttribute("user");
 	
-	HashMap<String, Double> quantRates = new HashMap<String, Double>();
+	Map<String, Double> quantRates = new HashMap<String, Double>();
 
+	Map<Currency, Double> rates = getFilteredRates("USD");
+	
 	for (Order order : osi.getAllOrdersNotUser(user, OrderStatus.ACTIVE, OrderStatus.PARTIALLY_COMPLETE)) {
-		Double t = ExchangeRate.getRateForPair(order.getBaseCurrency().toString(), order.getTargetCurrency().toString());
+		Double t = rates.get(order.getTargetCurrency()) / rates.get(order.getBaseCurrency()); // Calculates exchange rate from base to target.
 		String r = order.getBaseCurrency().toString()+ " to " + order.getTargetCurrency().toString();
 		Double quant = quantRates.get(r + " at " + t);
 		
